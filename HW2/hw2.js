c3697cbb4581e92529dff80b0b79b03842f80395
siphry <stacia.i.fry@gmail.com>
2018-10-02 17:07:38 -0700

    added lists to js file

---
+++ HW2/hw2.js
@@ -0,0 +1,4 @@
+var firstNames = ['Craven', 'Ahru', 'Sybil', 'Dracen', 'Elfin', 'Jeff', 'Zion', 'Fane', 'Lunas', 'Myst', 'Jahan', 'Shade', 'Hellis']
+var lastNames = ['Rubyellus', 'Javas', 'Pythonos', 'Fortranus', 'Deth', 'Haskellum', 'Seaquillis', 'Nekro', 'Moon', 'Drizzt', 'Ben-Mezd', 'Gruffen', 'Auros']
+var titles = ['the Crypt Destroyer', 'the Soul Devourer', 'the Shrieking Tomb', 'the Blind Prescence', 'the Silent Hunter', 'the Blood Syphon', 'the Black Rain', 
+'the Ghost Flayer', 'the Weeping Spirit', 'the Lost Child', 'the Fallen', 'the Normal']
