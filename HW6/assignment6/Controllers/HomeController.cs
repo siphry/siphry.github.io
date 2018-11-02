8c8d296ff0b1bf0c1abdf3411f522866f2c0d3c4
siphry <stacia.i.fry@gmail.com>
2018-11-01 19:08:39 -0700

    finished search/details page w/o styling

--- HW6/assignment6/Controllers/HomeController.cs
+++ HW6/assignment6/Controllers/HomeController.cs
@@ -3,28 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using assignment6.Models;
+using assignment6.Models.ViewModels;
 
 namespace assignment6.Controllers
 {
     public class HomeController : Controller
     {
+        private WorldWideImportersContext db = new WorldWideImportersContext();
+
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
-        }
+            string searchName = Request.QueryString["searchname"];
 
-        public ActionResult About()
+            List<PersonVM> result = db.People.Where(p => p.FullName.Contains(searchName)).Where(p => p.PersonID != 1).Select(p => new PersonVM { FullName = p.FullName, PreferredName = p.PreferredName, PhoneNumber = p.PhoneNumber, FaxNumber = p.FaxNumber, EmailAddress = p.EmailAddress, ValidFrom = p.ValidFrom}).ToList();
+
+            return View(result);
+        }
+       [HttpGet]
+       public ActionResult Details(string personName)
         {
-            ViewBag.Message = "Your application description page.";
+            List<PersonVM> person = db.People.Where(p => p.FullName == personName).Select(p => new PersonVM { FullName = p.FullName, PreferredName = p.PreferredName, PhoneNumber = p.PhoneNumber, FaxNumber = p.FaxNumber, EmailAddress = p.EmailAddress, ValidFrom = p.ValidFrom }).ToList();
 
-            return View();
+            return View(person);
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ HW6/assignment6/Models/ViewModels/PersonVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace assignment6.Models.ViewModels
+{
+    public class PersonVM
+    {
+        public string FullName { get; set; }
+        public string PreferredName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FaxNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime ValidFrom { get; set; }
+    }
+}
